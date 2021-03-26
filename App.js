d7b382b3c50cd8eb5fb52b581149b994f922f667
unknown <geric99997@gmail.com>
2021-03-25 18:27:05 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState}from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+import { io } from 'socket.io-client'
+const ENDPOINT = "http://192.168.1.111:8080";
 export default function App() {
+
+  const [response, setResponse] = useState("");
+
+  // useEffect(() => {
+  //   turnOn()
+  // },0);
+   const turnOn = () => {
+     fetch('http://192.168.1.111:8080', {
+       method: 'POST',
+       body: 'turnOn'
+     })
+   }
+   const turnOff = () => {
+    fetch('http://192.168.1.111:8080', {
+      method: 'POST',
+      body: 'turnOff'
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Turn on" onPress={turnOn} />
+      <Button title="Turn off" onPress={turnOff} />
     </View>
   );
 }
